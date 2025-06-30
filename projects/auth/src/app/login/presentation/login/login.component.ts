0b54174a73bd3a6189f06fa4a9051b4dcf7ba191
radoslawciupek <rciupek@maxto.pl>
2025-06-30 19:12:54 +0200

    Add login page

--- projects/auth/src/app/login/presentation/login/login.component.ts
+++ projects/auth/src/app/login/presentation/login/login.component.ts
@@ -1,18 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginForm } from '../../domain/model';
 import { LoginFacade } from './login-facade.service';
+import { MatCardModule } from '@angular/material/card';
+import { NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
   templateUrl: './login.component.html',
   standalone: true,
   providers: [LoginFacade],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   private readonly formBuilder = inject(FormBuilder);
   private readonly facade = inject(LoginFacade);
+
+  loginValid: boolean = true;
 
   readonly loginForm = this.formBuilder.group<LoginForm>({
     username: this.formBuilder.nonNullable.control('', [Validators.required]),
@@ -32,8 +45,14 @@ export class LoginComponent {
     this.facade
       .login({ username: formValue.username, password: formValue.password })
       .subscribe({
-        next: (response) => console.log('Login success', response.token),
-        error: (error) => console.log('Login error', error),
+        next: (response) => {
+          this.loginValid = false;
+          console.log('Login success', response.token);
+        },
+        error: (error) => {
+          this.loginValid = false;
+          console.log('Login error', error);
+        },
       });
   }
 }
